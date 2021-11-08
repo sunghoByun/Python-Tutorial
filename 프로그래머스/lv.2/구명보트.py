0f759dcdb173af641af0d731a68b3dc68ae35ce3
sungho <littlemon2@naver.com>
2021-11-08 23:58:49 +0900

    211108

--- 프로그래머스/lv.2/구명보트.py
+++ 프로그래머스/lv.2/구명보트.py
@@ -1,19 +1,6 @@
 people = [90,70,50,80,50,10]
 limit = 100
-answer = len(people)
-people2 = people
-# for x in range(len(people)-1):
-#
-#     maxNum = 0
-#     for y in people2:
-#         if y <= limit - people[x] and limit - people[x] > maxNum:
-#             maxNum = limit - y
-#
-#
-#     if maxNum != 0:
-#         people2.remove(y)
-#         answer -= 1
-
 
 
 print(answer)
+
